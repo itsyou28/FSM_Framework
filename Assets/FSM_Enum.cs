fc255f686530c95fb46fc5f0bc36e88544527b98
itsyou28 <itsyou28@gmail.com>
2017-12-17 17:16:34 +0900

    - Initilize
    Signed-off-by: itsyou28 <itsyou28@gmail.com>

---
+++ Assets/FSM_Enum.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace FiniteStateMachine
+{
+
+    public enum FSM_LAYER_ID
+    {
+        //레이어는 일반 배열을 사용하므로 Enum 변수는 반드시 0부터 순열로 정의되어야 한다. 
+        Main = 0,
+        UserStory,
+        MainUI,
+        Sub,
+        PopupUI
+    }
+
+    public enum FSM_ID
+    {
+        NONE = 0,
+        Main,
+    }
+
+    public enum TRANS_PARAM_ID
+    {
+        TRIGGER_NONE,
+        TRIGGER_RESET,
+        TRIGGER_ESCAPE,
+        TRIGGER_BACKBTN,
+        TRIGGER_NEXT,
+        TRIGGER_SUCCESS,
+        TRIGGER_FAIL,
+        TRIGGER_OPTION,
+        TRIGGER_CHECK_OK,
+        TRIGGER_CHECK_FAIL,
+        TRIGGER_YES,
+        TRIGGER_NO,
+        TRIGGER_SKIP,
+        TRIGGER_COMPLETE,
+        TRIGGER_CHECK_CONDITION,
+        TRIGGER_CHECK_ANY_CONDITION,
+        INT_USERSTORY_STATE,
+        INT_SELECT_MENU,
+        INT_USERSTORY_PRE_STATE,
+        INT_FLAG,
+    }
+
+    public enum STATE_ID
+    {
+        None = 0,
+        AnyState,
+        HistoryBack,
+
+    }
+
+
+    public enum TRANS_ID
+    {
+        NONE,
+        TIME,
+        HISTORY_BACK,
+        RESET,
+        ESCAPE_TO_MAIN,
+        BACK_TO_MAIN,
+        GAMEOVER,
+        ESCAPE,
+        SKIP,
+    }
+
+
+}
---
+++ Assets/FSM_Layer.cs
@@ -0,0 +1,454 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace FiniteStateMachine
+{
+    public class FSM_Layer
+    {
+        const int iMaxLayer = 5;
+
+        Dictionary<FSM_ID, FSM>[] dicFSM_EachLayer = new Dictionary<FSM_ID, FSM>[iMaxLayer];
+
+        FSM[] curFSM_EachLayer = new FSM[iMaxLayer];
+
+        event deleLayerPauseResume EventLayerPause;
+        event deleLayerPauseResume EventLayerResume;
+
+        Dictionary<FSM_LAYER_ID, List<deleStateTransEvent>> dicLayerChangeState = new Dictionary<FSM_LAYER_ID, List<deleStateTransEvent>>();
+
+        FSM_ID[] layerFSM_Buffer = new FSM_ID[iMaxLayer];
+
+        int layerNum;
+
+        int nParamBuffer;
+        float fParamBuffer;
+        bool bParamBuffer;
+        State tStateBuffer;
+        FSM tFSMBuffer;
+
+        public void ReleaseAll()
+        {
+            curFSM_EachLayer = new FSM[iMaxLayer];
+
+            for (int i = 0; i < iMaxLayer; i++)
+            {
+                if (dicFSM_EachLayer[i] != null)
+                    dicFSM_EachLayer[i].Clear();
+            }
+
+            dicFSM_EachLayer = new Dictionary<FSM_ID, FSM>[iMaxLayer];
+
+            EventLayerPause = null;
+            EventLayerResume = null;
+
+            dicLayerChangeState = new Dictionary<FSM_LAYER_ID, List<deleStateTransEvent>>();
+        }
+
+        public void AddFSM(FSM_LAYER_ID eLayer, FSM newFSM, FSM_ID id = FSM_ID.NONE)
+        {
+            layerNum = (int)eLayer;
+            if (dicFSM_EachLayer.Length <= layerNum)
+            {
+                UDL.LogError("할당되지 않은 레이어를 호출하고 있습니다 Layer 및 iMaxLayer를 확인해주세요", FSM.logOption, FSM.errorLoglv);
+                return;
+            }
+
+            if (dicFSM_EachLayer[layerNum] == null)
+                dicFSM_EachLayer[layerNum] = new Dictionary<FSM_ID, FSM>();
+
+            if (id != FSM_ID.NONE)
+            {
+                if (dicFSM_EachLayer[layerNum].ContainsKey(id))
+                {
+#if WARNING_FSM
+                UDL.LogWarning("동일 레이어에 중복된 FSM ID 를 등록하려함!", FSM.logOption, FSM.warningLoglv); 
+#endif
+                }
+                else
+                    newFSM.SetFSMID(id);
+            }
+
+            dicFSM_EachLayer[layerNum].Add(newFSM.fsmID, newFSM);
+
+#if DEBUG_FSM
+        UDL.Log(eLayer + " // add : " + newFSM.fsmID, FSM.logOption, 7);
+#endif
+
+            if (curFSM_EachLayer[layerNum] == null)
+            {
+                curFSM_EachLayer[layerNum] = newFSM;
+
+                RegisterToFSM_ChangeLayerState(eLayer);
+
+                curFSM_EachLayer[layerNum].Resume();
+            }
+            else
+                ChangeFSM(eLayer, newFSM.fsmID);
+        }
+
+        public void RemoveFSM(FSM_LAYER_ID eLayer, FSM_ID id)
+        {
+            layerNum = (int)eLayer;
+            if (dicFSM_EachLayer[layerNum].ContainsKey(id))
+            {
+                dicFSM_EachLayer[layerNum].Remove(id);
+            }
+#if WARNING_FSM
+        else
+            UDL.LogWarning("가지고 있지 않은 FSM을 삭제하려 함", FSM.logOption, FSM.warningLoglv); 
+#endif
+
+            if (curFSM_EachLayer[layerNum].fsmID == id)
+                curFSM_EachLayer[layerNum] = null;
+        }
+
+        public void SetInt_NoCondChk(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, int value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetInt_NoCondChk(paramID, value);
+        }
+
+        public void SetFloat_NoCondChk(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, float value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetFloat_NoCondChk(paramID, value);
+        }
+
+        public void SetBool_NoCondChk(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, bool value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetBool_NoCondChk(paramID, value);
+        }
+
+
+
+        public void SetInt(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, int value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetInt(paramID, value);
+        }
+
+        public void SetFloat(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, float value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetFloat(paramID, value);
+        }
+
+        public void SetBool(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID, bool value)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetBool(paramID, value);
+        }
+
+        public void SetTrigger(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].SetTrigger(paramID);
+        }
+
+
+
+        public int GetInt(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID)
+        {
+            if (!CurLayerCheck(eLayer))
+                return 0;
+            return curFSM_EachLayer[(int)eLayer].GetParamInt(paramID);
+        }
+
+        public float GetFloat(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID)
+        {
+            if (!CurLayerCheck(eLayer))
+                return 0;
+            return curFSM_EachLayer[(int)eLayer].GetParamFloat(paramID);
+        }
+
+        public bool GetBool(FSM_LAYER_ID eLayer, TRANS_PARAM_ID paramID)
+        {
+            if (!CurLayerCheck(eLayer))
+                return false;
+            return curFSM_EachLayer[(int)eLayer].GetParamBool(paramID);
+        }
+
+        bool CurLayerCheck(FSM_LAYER_ID eLayer)
+        {
+            if (curFSM_EachLayer[(int)eLayer] == null)
+            {
+#if WARNING_FSM
+            UDL.LogWarning(eLayer + " 지정한 레이어에 FSM이 지정되 있지 않음", FSM.logOption, FSM.warningLoglv); 
+#endif
+                return false;
+            }
+            else
+                return true;
+        }
+
+
+        public FSM GetCurFSM(FSM_LAYER_ID eLayer)
+        {
+            if (!CurLayerCheck(eLayer))
+                return null;
+
+            return curFSM_EachLayer[(int)eLayer];
+        }
+
+        public FSM GetFSM(FSM_LAYER_ID eLayer, FSM_ID fsmID)
+        {
+            layerNum = (int)eLayer;
+            if (dicFSM_EachLayer == null)
+                return null;
+
+            if (dicFSM_EachLayer[layerNum] == null)
+            {
+                UDL.LogError("지정한 레이어에 FSM이 지정되 있지 않음 ", FSM.logOption, FSM.errorLoglv);
+                return null;
+            }
+
+            if (!dicFSM_EachLayer[layerNum].TryGetValue(fsmID, out tFSMBuffer))
+            {
+                UDL.LogError(fsmID.ToString() + " FSM 이 등록되어 있지 않음", FSM.logOption, FSM.errorLoglv);
+                return null;
+            }
+
+            return dicFSM_EachLayer[layerNum][fsmID];
+        }
+
+        public State GetState(FSM_LAYER_ID eLayer, FSM_ID fsmID, STATE_ID stateID)
+        {
+            layerNum = (int)eLayer;
+            if (dicFSM_EachLayer == null)
+                return null;
+
+            if (dicFSM_EachLayer[layerNum] == null)
+            {
+                UDL.LogError("지정한 레이어에 FSM이 지정되 있지 않음 ", FSM.logOption, FSM.errorLoglv);
+                return null;
+            }
+
+            if (!dicFSM_EachLayer[layerNum].TryGetValue(fsmID, out tFSMBuffer))
+            {
+                UDL.LogError(fsmID.ToString() + " FSM 이 등록되어 있지 않음", FSM.logOption, FSM.errorLoglv);
+                return null;
+            }
+
+            return dicFSM_EachLayer[layerNum][fsmID].GetState(stateID);
+        }
+
+        public State GetCurState(FSM_LAYER_ID eLayer)
+        {
+            layerNum = (int)eLayer;
+            if (curFSM_EachLayer[layerNum] == null)
+                return null;
+
+            return curFSM_EachLayer[layerNum].GetCurState();
+        }
+
+        public STATE_ID GetCurStateID(FSM_LAYER_ID eLayer)
+        {
+            layerNum = (int)eLayer;
+            if (curFSM_EachLayer[layerNum] == null)
+                return STATE_ID.None;
+
+            return curFSM_EachLayer[layerNum].GetCurStateID();
+        }
+
+        public void HistoryBack(FSM_LAYER_ID eLayer)
+        {
+            if (CurLayerCheck(eLayer))
+                curFSM_EachLayer[(int)eLayer].HistoryBack();
+        }
+
+        public void ChangeFSM(FSM_LAYER_ID eLayer, FSM_ID fsmID)
+        {
+            layerNum = (int)eLayer;
+
+#if WARNING_FSM
+        if (!CurLayerCheck(eLayer))
+        {
+            UDL.LogWarning("Fail ChangeFSM. Layer : " + eLayer, FSM.logOption, FSM.warningLoglv);
+            return;
+        }
+
+        if (!dicFSM_EachLayer[layerNum].TryGetValue(fsmID, out tFSMBuffer))
+        {
+            UDL.LogWarning(eLayer + " 에 " + fsmID + " 가 등록되어 있지 않습니다", FSM.logOption, FSM.warningLoglv);
+            return;
+        } 
+#endif
+
+            if (CurLayerCheck(eLayer))
+            {
+                layerFSM_Buffer[layerNum] = curFSM_EachLayer[layerNum].fsmID;
+
+                curFSM_EachLayer[layerNum].Pause();
+
+                UnRegisterToFSM_ChangeLayerState(eLayer);
+
+                curFSM_EachLayer[layerNum] = dicFSM_EachLayer[layerNum][fsmID];
+
+                RegisterToFSM_ChangeLayerState(eLayer);
+
+                curFSM_EachLayer[layerNum].Resume();
+            }
+#if DEBUG_FSM
+        UDL.Log("(ChangeFSM) curFSM : " + curFSM_EachLayer[layerNum].fsmID.ToString(), FSM.logOption, 7); 
+#endif
+        }
+
+        public void ChangeFSM_Manual(FSM_LAYER_ID eLayer, FSM_ID fsmID)
+        {
+            layerNum = (int)eLayer;
+            if (CurLayerCheck(eLayer))
+            {
+                layerFSM_Buffer[layerNum] = curFSM_EachLayer[layerNum].fsmID;
+
+                UnRegisterToFSM_ChangeLayerState(eLayer);
+
+                curFSM_EachLayer[layerNum] = dicFSM_EachLayer[layerNum][fsmID];
+
+                RegisterToFSM_ChangeLayerState(eLayer);
+            }
+        }
+
+        public void ChangeFSM_BufferBack(FSM_LAYER_ID eLayer)
+        {
+            ChangeFSM(eLayer, layerFSM_Buffer[layerNum]);
+        }
+
+#if WARNING_FSM
+     string[] errMsgAbout_Register_ChangeLayerState =
+{
+        "Success",
+        "해당 레이어에 FSM이 등록되어 있지 않습니다. ",
+        "해당 레이어의 이벤트 버퍼 리스트가 존재하지 않습니다"
+    }; 
+#endif
+
+        private int RegisterToFSM_ChangeLayerState(FSM_LAYER_ID eLayer)
+        {
+            layerNum = (int)eLayer;
+            if (!CurLayerCheck(eLayer))
+                return 1;
+
+            UnRegisterToFSM_ChangeLayerState(eLayer);//중복등록을 방지하기 위해 호출한다. 
+
+            if (!dicLayerChangeState.ContainsKey(eLayer))
+                return 2;
+
+            for (int idx = 0; idx < dicLayerChangeState[eLayer].Count; idx++)
+                curFSM_EachLayer[layerNum].EventStateChange += dicLayerChangeState[eLayer][idx];
+
+            return 0;
+        }
+
+        private int UnRegisterToFSM_ChangeLayerState(FSM_LAYER_ID eLayer)
+        {
+            if (!CurLayerCheck(eLayer))
+                return 1;
+
+            if (!dicLayerChangeState.ContainsKey(eLayer))
+                return 2;
+
+            for (int idx = 0; idx < dicLayerChangeState[eLayer].Count; idx++)
+                curFSM_EachLayer[layerNum].EventStateChange -= dicLayerChangeState[eLayer][idx];
+
+            return 0;
+        }
+
+        public void RegisterEventChangeLayerState(FSM_LAYER_ID eLayer, deleStateTransEvent _deleFunc)
+        {
+            layerNum = (int)eLayer;
+            if (layerNum >= iMaxLayer)
+            {
+                UDL.LogError("할당 되지 않은 레이어를 호출했습니다", FSM.logOption, FSM.errorLoglv);
+                return;
+            }
+
+            if (!dicLayerChangeState.ContainsKey(eLayer))
+                dicLayerChangeState.Add(eLayer, new List<deleStateTransEvent>());
+
+            dicLayerChangeState[eLayer].Add(_deleFunc);
+
+
+#if WARNING_FSM
+        int result = RegisterToFSM_ChangeLayerState(eLayer);
+
+        if (result == 1)
+            UDL.LogWarning(eLayer + " " + errMsgAbout_Register_ChangeLayerState[result] + " // 이 후 해당 레이어에 ChangeFSM이 호출 될 때 반영될 수 있습니다. ", FSM.logOption, FSM.warningLoglv); 
+#else
+            RegisterToFSM_ChangeLayerState(eLayer);
+#endif
+        }
+
+        public void UnRegisterChangeLayerState(FSM_LAYER_ID eLayer, deleStateTransEvent _deleFunc)
+        {
+            if (!dicLayerChangeState.ContainsKey(eLayer))
+                return;
+
+            dicLayerChangeState[eLayer].Remove(_deleFunc);
+
+
+            foreach (Dictionary<FSM_ID, FSM> dic in dicFSM_EachLayer)
+            {
+                foreach (FSM fsm in dic.Values)
+                    fsm.EventStateChange -= _deleFunc;
+            }
+        }
+
+        public void RegisterPauseEvent(deleLayerPauseResume _deleFunc)
+        {
+            EventLayerPause += _deleFunc;
+        }
+
+        public void UnRegisterPauseEvent(deleLayerPauseResume _deleFunc)
+        {
+            EventLayerPause -= _deleFunc;
+        }
+
+        public void RegisterResumeEvent(deleLayerPauseResume _deleFunc)
+        {
+            EventLayerResume += _deleFunc;
+        }
+
+        public void UnRegisterResumeEvent(deleLayerPauseResume _deleFunc)
+        {
+            EventLayerResume -= _deleFunc;
+        }
+
+        public void Pause(FSM_LAYER_ID eLayer)
+        {
+            if (!CurLayerCheck(eLayer))
+                return;
+
+            curFSM_EachLayer[layerNum].Pause();
+
+            if (EventLayerPause != null)
+                EventLayerPause(eLayer);
+        }
+
+        public void Resume(FSM_LAYER_ID eLayer)
+        {
+            if (!CurLayerCheck(eLayer))
+                return;
+
+            curFSM_EachLayer[layerNum].Resume();
+
+            if (EventLayerResume != null)
+                EventLayerResume(eLayer);
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < iMaxLayer; i++)
+            {
+                if (curFSM_EachLayer[i] != null)
+                    curFSM_EachLayer[i].TimeCheck();
+            }
+        }
+
+        public void ReleaseLayer(FSM_LAYER_ID eLayer)
+        {
+            dicFSM_EachLayer[layerNum].Clear();
+            curFSM_EachLayer[layerNum] = null;
+        }
+    }
+}
