950a623ec1a4d2d052b397aaff8a3dba75f6da3b
itsyou28 <itsyou28@gmail.com>
2018-02-10 21:53:18 +0900

    UIBind 관련 소스 파일 경로 정리

---
+++ Assets/Script/UIBindSample/UI_Float_Setter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_Float_Setter : UIBindF
+{
+    [SerializeField]
+    Slider slider;
+
+    protected override void OnDataChange()
+    {
+        base.OnDataChange();
+        
+        if(!Mathf.Approximately(slider.value, bindedData.Value))
+            slider.value = bindedData.Value;
+    }
+
+    public void OnValueChange(float fValue)
+    {
+        bindedData.Value = fValue;
+    }
+}
